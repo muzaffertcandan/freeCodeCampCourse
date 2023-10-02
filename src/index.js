56c1bdb7c14761e382295ecfa92baa1ae3d2f56d
muzaffertcandan <muzaffertcandan@gmail.com>
2023-10-02 18:52:52 +0300

    till es6

--- src/index.js
+++ src/index.js
@@ -8,15 +8,15 @@ const books = [
     author: "James Clear",
     title: "Atomic Habits",
     img: "./images/atomic.jpg",
+    id: 1,
   },
   {
     author: "Rick Riordan",
-    title: "Percy Jackson: The Demigod",
+    title: "Percy Jackson",
     img: "./images/percy.jpg",
+    id: 2,
   },
 ];
- 
-
 
 // const names = ["john","peter", "george"]
 // const newNames = names.map((item)=>{
@@ -24,22 +24,44 @@ const books = [
 // })
 
 const BookList = () => {
+  const myBook = "Atomic Habits";
+  const getBook = (id) => {
+    const book = books.find((book) => book.id === id);
+    console.log(book);
+  };
+  const someValue = "valueee";
+  const displayValue = () => {
+    // console.log(someValue);
+  };
   return (
     <section className="booklist">
-     {books.map(()=>{
-      return <div><h2></h2></div>
-     })}
+      {/* <EventExamples /> */}
+
+      {books.map((book) => {
+        // const { img, title, author, id } = book;
+        return (
+          <Book
+            {...book}
+            key={book.id}
+            getBook={getBook}
+            displayValue={displayValue}
+          />
+        );
+      })}
     </section>
   );
 };
 
 const Book = (props) => {
-  console.log(props);
-  const { img, title, author } = props;
+  const { img, title, author, getBook, id } = props;
+  const getSingleBook = () =>{
+    getBook(id)
+  }
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
+      <button onClick={()=>getBook(id)}>click</button>
       <h4>{author}</h4>
     </article>
   );
